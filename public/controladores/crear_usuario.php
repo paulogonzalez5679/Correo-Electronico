25b2a424aeaf9e9b58406761668b29236d3c8b63
paulogonzalez5679 <paulomga10@gmail.com>
2019-05-22 23:22:14 -0500

    eliminar mensaje y usuario listos

--- public/controladores/crear_usuario.php
+++ public/controladores/crear_usuario.php
@@ -2,17 +2,19 @@
 <html> 
 <head> 
 <meta charset="UTF-8"> 
-    <title> Nuevo REGISTRO </title> 
+    <title>Crear Nuevo Usuario</title> 
 </head> 
 <body> 
  
-    <?php                  
+    <?php         
+        //incluir conexión a la base de datos         
         include '../../config/conexionBD.php';                 
  
         $nombres = isset($_POST["nombres"]) ? mb_strtoupper(trim($_POST["nombres"]), 'UTF-8') : null; 
         $apellidos = isset($_POST["apellidos"]) ? mb_strtoupper(trim($_POST["apellidos"]), 'UTF-8') : null;         
         $correo = isset($_POST["correo"]) ? trim($_POST["correo"]): null;         
         $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
+
         $fotoNombre = $_FILES["foto"]["name"];
         $fotoRuta = $_FILES["foto"]["tmp_name"];
         $fotoDestino = "../../fotos/".$fotoNombre;
@@ -23,7 +25,7 @@
         $sql = "INSERT INTO usuario VALUES (0, '$nombres', '$apellidos', '$correo', MD5('$contrasena'), '$rol', 0, null, null, '$fotoDestino')";         
  
         if ($conn->query($sql) === TRUE) {             
-            echo "<p>REGISTRADO</p>";      
+            echo "<p>Se ha creado los datos personales correctamemte!!!</p>";      
         } else {             
             if($conn->errno == 1062){                 
                 echo "<p class='error'>La persona con la cedula $cedula ya esta registrada en el sistema </p>";
@@ -32,9 +34,8 @@
             }             
         } 
          
-        
-        $conn->close();         
-        echo "<a href='../vista/crear_usuario.html'>Regresar</a>"; 
+        //cerrar la base de datos 
+        $conn->close();         echo "<a href='../vista/crear_usuario.html'>Regresar</a>"; 
     ?> 
  
 </body> 
